edf9aafa841aede432fcc7b9ee41bd25f77b6b24
Matthis Perrin <matthis.perrin@gmail.com>
2015-11-04 17:16:04 -0800

    Finish webhook endpoint

--- app/api/webhook.py
+++ app/api/webhook.py
@@ -26,22 +26,56 @@ def webhook_receive(context, request):
     from_addresses = reduce(lambda all_addresses, input: all_addresses + input.get('addresses'), inputs, [])
     to_addresses = reduce(lambda all_addresses, output: all_addresses + output.get('addresses'), outputs, [])
 
-    # TODO - This is slow and flaky... Need something else
-    for i in xrange(100):
-        next_address = generate_address_from_xpub(xpub, index=i)
-        if next_address in from_addresses:
-            receive = False
-            break
-        elif next_address in to_addresses:
-            receive = True
-            break
+    from_addresses_info = AddressLookup.multi_get(user_id, from_addresses)
+    to_addresses_info = AddressLookup.multi_get(user_id, to_addresses)
 
-    addresses_info = AddressLookup.multi_get(user_id, from_addresses + to_addresses)
-    print addresses_info
+    amount_spent = 0
+    amount_received = 0
 
-    # if receive is None:
-    #     return
-    #
-    # addresses = from_addresses if receive else to_addresses
-    # index = addresses.index(next_address)
-    # # amount = addresses[index].get('')
+    # Look at the oldest address found, if we don't have a lookup for the next 10 addresses, we generate them
+    # TODO - Some of those addresses might have a different xpub, we should get the oldest for each xpub
+    #        and generate the address lookup for each.
+    if len(from_addresses_info) + len(to_addresses_info) > 0:
+        oldest = reduce(lambda best, address: address if address.offset > best.offset else best, from_addresses_info + to_addresses_info)
+        max = AddressLookup.get_oldest(user_id, oldest.xpub)
+        if (oldest.offset > max.offset - 10):
+            sub_addresses = []
+            indexes = []
+            for i in xrange(oldest.offset + 1, oldest.offset + 11):
+                sub_address = generate_address_from_xpub(oldest.xpub, i)
+                sub_addresses.append(sub_address)
+                indexes.append(i)
+            AddressLookup.multi_add(user_id, sub_addresses, indexes, oldest.wallet_name, oldest.xpub)
+
+            # Do one more round-trip to the database now that we have more data
+            from_addresses_info = AddressLookup.multi_get(user_id, from_addresses)
+            to_addresses_info = AddressLookup.multi_get(user_id, to_addresses)
+
+
+        # Create arrays of addresses owned by the user
+        def io_has_address(io, addresses):
+            for address in io.get('addresses'):
+                if address in addresses:
+                    return True
+            return False
+        from_addresses_owned = [info.address for info in from_addresses_info]
+        to_addresses_owned = [info.address for info in to_addresses_info]
+
+        # Filter the inputs and outputs owned by the user
+        inputs_owned = filter(lambda input: io_has_address(input, from_addresses_owned), inputs)
+        outputs_owned = filter(lambda output: io_has_address(output, to_addresses_owned), outputs)
+
+        # Gather amount spent and received (in satoshi)
+        amount_spent = sum([input.get('output_value') for input in inputs_owned])
+        amount_received = sum([output.get('value') for output in outputs_owned])
+    else:
+        # We know nothing about the inputs or outputs, nothing we can do...
+        return
+
+    diff = amount_received - amount_spent
+    if diff > 0:
+        print 'Received {} satoshis'.format(diff)
+    elif diff < 0:
+        print 'Sent {} satoshis'.format(-diff)
+    elif amount_received > 0:
+        print 'Sent {} satoshis to yourself'
--- app/db/address_lookup.py
+++ app/db/address_lookup.py
@@ -18,21 +18,37 @@ class AddressLookup:
     @staticmethod
     def get(user_id, address):
         """
-        Returns all the info of where the address is coming from (restricted to a specific user_id)
+        Returns all the info of where the address is coming from (restricted to a specific `user_id`)
         """
-        res = MySQL.run('SELECT * FROM address_lookup WHERE user_id = {} AND address = {}'.format(user_id, address))
+        res = MySQL.run('''
+          SELECT address, user_id, wallet_name, xpub, offset, creation_time FROM address_lookup
+          WHERE user_id = {} AND address = {}'''.format(user_id, address))
         if len(res) == 0:
             return None
         return AddressLookup(*res[0])
 
     @staticmethod
+    def get_oldest(user_id, xpub):
+        """
+        Returns the info of the oldest address for a specific `user_id` and `xpub`
+        """
+        res = MySQL.run('''
+          SELECT address, user_id, wallet_name, xpub, offset, creation_time FROM address_lookup
+          WHERE user_id = {} AND xpub = '{}' ORDER BY offset DESC LIMIT 1'''.format(user_id, xpub))
+        if len(res) == 0:
+            return None
+        return AddressLookup(*res[0])
+
+
+    @staticmethod
     def multi_get(user_id, addresses):
         """
-        Returns the info of all the keys that we have in the lookup table
+        Returns the info of all the `addresses` that we have in the lookup table (restricted to a specific `user_id`)
         """
         addresses = "', '".join(addresses)
-        res = MySQL.run("SELECT * FROM address_lookup WHERE user_id = {} AND address IN ('{}')"
-                        .format(user_id, addresses))
+        res = MySQL.run('''
+          SELECT address, user_id, wallet_name, xpub, offset, creation_time FROM address_lookup
+          WHERE user_id = {} AND address IN ('{}')'''.format(user_id, addresses))
         return [AddressLookup(*address_info) for address_info in res]
 
     @staticmethod
